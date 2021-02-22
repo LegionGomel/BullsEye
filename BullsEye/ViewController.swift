a92e1003a81ac1bf3b99476ede2585106240dfb7
Yuri Ershov <shtorm0606@gmail.com>
2021-02-22 18:05:11 +0300

    Finish start new round function, add random num variable

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -8,23 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var sliderValue: UISlider!
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let roundedValue = sliderValue.value.rounded()
+        currentValue = Int(roundedValue)
+        startNewRound()
     }
 
     @IBAction func hitButtonPressed(_ sender: Any) {
         
+        let message = "Current slider value is: \(currentValue)" +
+        "\nThe target value is: \(targetValue)"
         // Create popup alert controller
-        let alert = UIAlertController(title: "Hello world!", message: "This is my first app", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Hello world!", message: message, preferredStyle: .alert)
         // Create popup alert action (a button inside)
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         // Add button to popup
         alert.addAction(action)
         // Show popup alert when hitButtonPressed
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        let roundedValue = sliderValue.value.rounded()
+        currentValue = Int(roundedValue)
+        
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        sliderValue.value = Float(currentValue)
+    }
 }
 
