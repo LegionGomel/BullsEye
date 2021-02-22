496c33b333c581c9f446e9710bb49fa12b3b655f
Yuri Ershov <shtorm0606@gmail.com>
2021-02-22 21:55:40 +0300

    Set Webview instead of simple label in game info

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -6,11 +6,23 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
 
+    @IBOutlet weak var webViewOutlet: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+            // Method to show html file in bundle instead simple label
+        if let htmlPath = Bundle.main.path(forResource: "BullsEye", ofType: "html") {
+            let url = URL(fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webViewOutlet.load(request)
+        }
+
+        
 
     }
 
